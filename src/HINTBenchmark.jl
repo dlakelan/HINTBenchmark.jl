4deb34149974490f4686f6fcaf5224da0781001f
Daniel Lakeland <dlakelan@street-artists.org>
2022-10-05 09:33:00 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/HINTBenchmark.jl
@@ -0,0 +1,5 @@
+module HINTBenchmark
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HINTBenchmark
+using Test
+
+@testset "HINTBenchmark.jl" begin
+    # Write your tests here.
+end
